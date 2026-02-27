8b9c562d5e4461682d311f550f6a0ce8432626e2
Paul John toral <pjtoralme@gmail.com>
2026-02-27 13:23:01 +0800

    fix: added comment to increment method

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // This increments the counter
   void _incrementCounter() {
     setState(() {
       _counter++;
